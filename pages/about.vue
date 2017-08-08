bec03622b0e2ffc2d301c043ef16b8c254e4681e
sunya9 <mugen.xyz@gmail.com>
2017-08-08 20:07:08 +0900

    Add rules and about

--- pages/about.vue
+++ pages/about.vue
@@ -1,5 +1,30 @@
 <template>
   <div class="content">
     <h1>About CSS Frameworks</h1>
+    <p>CSS Frameworks is leaderboard of the CSS frameworks. Information is Collected using API from Github.</p>
+    <p>
+      This site itself uses <a href="http://bulma.io/">bulma</a>.
+    </p>
+    <p>
+      Hosting service is <a href="https://www.netlify.com/">Netlify</a>,
+      it is running <a href="https://www.staticgen.com/">StaticGen</a>.
+      I thought necessary the site that compare their CSS frameworks when I seen it. So, This site was created.
+      Thank StaticGen and pay one's respects.
+    </p>
   </div>
 </template>
+<script>
+export default {
+  data () {
+    return {
+      title: 'About CSS Frameworks'
+    }
+  },
+  head () {
+    return {
+      title: this.title
+    }
+  }
+}
+</script>
+
--- pages/rules.vue
+++ pages/rules.vue
@@ -1,5 +1,25 @@
 <template>
   <div class="content">
-    <h1>Rules</h1>
+    <h1>{{title}}</h1>
+    <p>
+      Following bellow rules.
+    </p>
+    <ul>
+      <li>Don't dependant specific JavaScript frameworks. But exclude jQuery. It is the library rather than the framework.</li>
+    </ul>
   </div>
 </template>
+<script>
+export default {
+  data () {
+    return {
+      title: 'The Rules[WIP]'
+    }
+  },
+  head () {
+    return {
+      title: this.title
+    }
+  }
+}
+</script>
