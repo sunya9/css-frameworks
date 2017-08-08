7706171631bbff329c2b28790bd6a07470f5d929
sunya9 <mugen.xyz@gmail.com>
2017-08-08 15:21:51 +0900

    FIx fetching script

--- scripts/get-repo-data.js
+++ scripts/get-repo-data.js
@@ -2,29 +2,73 @@ const fs = require('fs')
 const path = require('path')
 const request = require('request-promise')
 const repos = require('./repos')
+require('dotenv').config()
 
-const END_POINT = 'https://api.github.com/repos/'
-
-const reposPromise = Object.keys(repos).map(async name => {
-  const repo = repos[name]
-  return {
-    name,
-    data: await request(END_POINT + repo, {
-      headers: {
-        'User-Agent': 'request module',
-        Accept: 'application/vnd.github.drax-preview+json'
-
-      }
-    }).catch(console.error)
+class ReposData {
+  static get END_POINT () {
+    return 'https://api.github.com/repos/'
   }
-})
 
-Promise.all(reposPromise).then(repoData => {
-  const storePath = path.join(__dirname, '../data.json')
-  repoData = repoData.map(repo => {
-    return Object.assign(repo, {
-      data: JSON.parse(repo.data)
+  static get GITHUB_TOKEN () {
+    if (!process.env.GITHUB_TOKEN) {
+      throw new Error('You must set GITHUB_TOKEN environment variable: GITHUB_TOKEN=<username>:<token>')
+    } else {
+      return process.env.GITHUB_TOKEN
+    }
+  }
+
+  static get USER_AND_PASS () {
+    const res = ReposData.GITHUB_TOKEN.split(':')
+    if (res.length === 2) {
+      return res
+    } else {
+      throw new Error('GITHUB_TOKEN requires the following format separated colon: <username>:<token>')
+    }
+  }
+
+  fetchAllAndStore () {
+    return this.fetchAll().then(this.store)
+  }
+
+  fetchAll () {
+    const reposPromise = repos.map(this.fetch)
+    return Promise.all(reposPromise)
+  }
+
+  async fetch (repo) {
+    const [user, pass] = ReposData.USER_AND_PASS
+    return {
+      name: repo.name,
+      data: await request(ReposData.END_POINT + repo.repo, {
+        headers: {
+          'User-Agent': 'CSS Frameworks',
+          // Required for getting license information.
+          Accept: 'application/vnd.github.drax-preview+json'
+        },
+        json: true,
+        auth: {
+          user, pass
+        }
+      }).catch(err => {
+        console.error(err)
+        process.exit(1)
+      })
+    }
+  }
+
+  store (repoData) {
+    const storePath = path.join(__dirname, '../data.json')
+    const writeData = JSON.stringify(repoData)
+    return new Promise((resolve, reject) => {
+      fs.writeFile(storePath, writeData, err =>
+        !err ? resolve() : reject(err))
     })
-  })
-  fs.writeFileSync(storePath, JSON.stringify(repoData))
-})
+  }
+}
+
+new ReposData()
+  .fetchAllAndStore()
+  .catch(console.error)
+
+// for test
+module.exports = ReposData
---
+++ scripts/repos.js
@@ -0,0 +1,33 @@
+module.exports = [
+  {
+    name: 'Bootstrap',
+    repo: 'twbs/bootstrap'
+  }, {
+    name: 'Bulma',
+    repo: 'jgthms/bulma'
+  }, {
+    name: 'Foundation',
+    repo: 'zurb/foundation-sites'
+  }, {
+    name: 'Material Design Lite',
+    repo: 'google/material-design-lite'
+  }, {
+    name: 'Spectre.css',
+    repo: 'picturepan2/spectre'
+  }, {
+    name: 'Milligram',
+    repo: 'milligram/milligram'
+  }, {
+    name: 'UIkit',
+    repo: 'uikit/uikit'
+  }, {
+    name: 'Materialize',
+    repo: 'Dogfalo/materialize'
+  }, {
+    name: 'siimple',
+    repo: 'siimple/siimple'
+  }, {
+    name: 'pure',
+    repo: 'yahoo/pure'
+  }
+]
